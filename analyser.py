f049769e1fd650d90ec96b995dd10cce77846881
gredas <bartlomiej.greda@gmail.com>
2023-04-19 13:00:17 +0200

    19.04.2023 BG

--- analyser.py
+++ analyser.py
@@ -10,7 +10,7 @@ opinions = pd.read_json(f"./opinions/{product_code}.json")
 print(opinions)
 
 opinions_count = len(opinions.index) #opinions.shape[0]
-pros_count =0
-cons_count =0
+pros_count = sum([False if len(p)==0 else True for p in opinions.pros])
+cons_count = sum([False if len(c)==0 else True for c in opinions.cons])
 average_score =0
 print(opinions["pros"])
